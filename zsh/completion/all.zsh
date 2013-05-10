3a0cc55c1f735078680ffe405b238f858b4e8329
Brian Moyles <bmoyles@blackpearlsystems.com>
2013-05-10 01:57:52 -0700

    completion, prompt newline

---
+++ zsh/completion/all.zsh
@@ -0,0 +1,13 @@
+autoload -U compinit
+compinit
+
+zstyle ':completion:*' rehash true
+
+# load more complex completions if they exist
+local compdir=${ZSH}/completion
+for file in ${COMPDIR}/*/*.zsh(N)
+do
+    source ${file}
+done
+
+# vim: filetype=zsh:ts=4:sw=4:expandtab
