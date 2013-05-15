7f3f0ea69f759789162a2abc1be0fa2b5718b02f
Brian Moyles <bmoyles@blackpearlsystems.com>
2013-05-15 13:22:50 -0700

    global tmx alias

--- zsh/aliases/all.zsh
+++ zsh/aliases/all.zsh
@@ -2,6 +2,6 @@ alias vi=vim
 alias sa='ssh-add ${HOME}/.ssh/id_rsa'
 alias sl='ssh-add -l'
 alias ls="ls ${LS_OPTIONS}"
-alias tmux="tmux -f ${TMUXRC}"
+alias -g tmux="tmux -f ${TMUXRC}"
 
 # vim: filetype=zsh:ts=4:sw=4:expandtab
