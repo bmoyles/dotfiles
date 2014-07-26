19097776548885dc4cf6d17e8ee708809f60f8d9
Brian Moyles <brian@blackpearlsystems.com>
2014-07-26 15:01:02 -0700

    ...

--- zsh/editor/all.zsh
+++ zsh/editor/all.zsh
@@ -4,21 +4,16 @@ autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '^f' edit-command-line
 
-function rationalize-dot {
-    if [[ $LBUFFER = *.. ]] ; then
-        LBUFFER+=/..
-    else
-        LBUFFER+=.
-    fi
-}
-zle -N rationalize-dot
-bindkey . rationalize-dot
-
 function insert_sudo {
-    zle beginning-of-line
-    zle -U 'sudo '
+  zle beginning-of-line
+  zle -U 'sudo '
 }
 zle -N insert-sudo insert_sudo
 bindkey '^[s' insert-sudo
 
-# vim: filetype=zsh:ts=4:sw=4:expandtab
+if [ -e /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+  export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
+  source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+
+# vim: filetype=zsh:ts=2:sw=2:expandtab
