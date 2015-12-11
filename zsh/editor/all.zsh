1c3734fa862a3b97055805036d69a7f97418a5cc
Brian Moyles <bmoyles@netflix.com>
2015-12-10 18:38:44 -0800

    restart zsh fun

--- zsh/editor/all.zsh
+++ zsh/editor/all.zsh
@@ -11,6 +11,9 @@ function insert_sudo {
 zle -N insert-sudo insert_sudo
 bindkey '^[s' insert-sudo
 
+#zle -N restart-zsh restart_zsh
+bindkey -s '^[r' 'restart_zsh\n'
+
 if [ -e /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
   export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
   source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
