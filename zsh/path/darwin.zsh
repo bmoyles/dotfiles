25f94f2883af782837d31bafff6fcc067ffc7151
Brian Moyles <bmoyles@netflix.com>
2015-12-17 15:08:42 -0800

    mac-specific path

---
+++ zsh/path/darwin.zsh
@@ -0,0 +1,17 @@
+typeset -Ux path manpath
+
+path=(
+  /usr/local/opt/coreutils/libexec/gnubin(N/)
+  /usr/local/opt/gnu-tar/libexec/gnubin(N/)
+  /usr/local/opt/gnu-sed/libexec/gnubin(N/)
+  /usr/local/opt/ruby/bin(N/)
+  /opt/X11/bin(N/)
+  ${path}
+)
+
+manpath=(
+  /usr/local/opt/coreutils/libexec/gnuman(N/)
+  /usr/local/opt/gnu-tar/share/man(N/)
+  /usr/local/opt/gnu-sed/libexec/gnuman(N/)
+  ${manpath}
+)
