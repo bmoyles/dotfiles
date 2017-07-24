279005d2508d96045e655e1e5e1624168bbc91ec
Brian Moyles <bmoyles@netflix.com>
2017-07-24 10:45:52 -0700

    python user base cleanup (mac uses a framework build and thus does not create version-specific site-packages directories if pythonuserbase is overridden making py2 and py3 comingling a problem). clean up powerline prompt some, ensure prompt_bang is on

--- zsh/env/python/python.zsh
+++ zsh/env/python/python.zsh
@@ -1,4 +1,3 @@
 export VIRTUAL_ENV_DISABLE_PROMPT="disable"
-export PYTHONUSERBASE=${DOTLOCAL}
 
 # vim: ft=zsh ts=2 sts=2 sw=2 expandtab
--- zsh/path/darwin.zsh
+++ zsh/path/darwin.zsh
@@ -13,3 +13,21 @@ manpath=(
   /usr/local/opt/gnu-sed/libexec/gnuman(N/)
   ${manpath}
 )
+
+if (( ${+commands[python3]} )); then
+  _py_user_base="$(python3 -msite --user-base)/bin"
+  path=(
+    ${_py_user_base}(N/)
+    ${path}
+  )
+  unset _py_user_base
+fi
+
+if (( ${+commands[python2]} )); then
+  _py_user_base="$(python2 -msite --user-base)/bin"
+  path=(
+    ${_py_user_base}(N/)
+    ${path}
+  )
+  unset _py_user_base
+fi
